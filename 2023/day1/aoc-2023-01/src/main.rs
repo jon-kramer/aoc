f285f658eb8ac8490d14a7421eda4d2edd0f909f
Jonathan Kramer <jonathan.paul.kramer@gmail.com>
2023-12-08 13:26:55 +0100

    AOC 2023 day1 part2 solution

--- 2023/day1/aoc-2023-01/src/main.rs
+++ 2023/day1/aoc-2023-01/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
                 if n == 0 {
                     break;
                 } else {
-                    let calibration = calculate_calibration(buffer.trim());
+                    let calibration = calculate_calibration2(buffer.trim());
                     sum += calibration;
                     writeln!(out, "{}", calibration).unwrap();
                     // let _ = out.flush();
@@ -44,6 +44,60 @@ fn calculate_calibration(line: &str) -> u32 {
                 found_num = true;
             }
             n2 = num;
+        }
+    }
+
+    if !found_num {
+        panic!("BAD INPUT!")
+    }
+
+    (n1 * 10) + n2
+}
+
+/*
+* "soneighthreeight"
+*
+* Loop through characters, checking first for ascii digits and then for substring matches
+* against the valid string list.
+*
+* Once a substring is found, we can convert it to its numeric representation and set our n1,n2
+* values appropriately.
+*
+* what about "oneight"? is the answer 11, 88, or 18?
+* - for now this will be 18
+*/
+
+const WORD_DIGITS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+fn calculate_calibration2(line: &str) -> u32 {
+    let mut found_num: bool = false;
+    let mut n1: u32 = 10;
+    let mut n2: u32 = 10;
+    for (i, char) in line.chars().enumerate() {
+        // Attempt to find a digit, first checking ascii character, then checking for substring
+        // matches
+        let mut num: Option<u32> = None;
+        if char.is_ascii_digit() {
+            num = Some(char as u32 - '0' as u32);
+        }
+        // Get substring of buffer starting from this char and check if it begins with any of the DIGITS
+        else {
+            // get slice from index
+            // for each DIGIT, begins_with, and if we find one, convert to num and set, else
+            for (j, word_digit) in WORD_DIGITS.iter().enumerate() {
+                if line[i..].starts_with(word_digit) {
+                    num = Some(j as u32);
+                }
+            }
+        }
+
+        if let Some(n) = num {
+            if !found_num {
+                n1 = n;
+                found_num = true;
+            }
+            n2 = n;
         }
     }
 
